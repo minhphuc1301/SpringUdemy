79c58b22c3d6021e9bd73c078151ba156e06815c
minhphuc1301 <nhoclunway@gmail.com>
2023-06-21 17:01:19 +0700

    fix

--- mycoolapp/src/main/java/com/fptudemy/mycoolapp/mycoolapp/controller/TodoController.java
+++ mycoolapp/src/main/java/com/fptudemy/mycoolapp/mycoolapp/controller/TodoController.java
@@ -1,2 +1,36 @@
-package com.fptudemy.mycoolapp.mycoolapp.controller;public class TodoController {
+package com.fptudemy.mycoolapp.mycoolapp.controller;
+
+import com.fptudemy.mycoolapp.mycoolapp.models.Todo;
+import com.fptudemy.mycoolapp.mycoolapp.services.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/TodoController")
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+
+    @GetMapping("")
+    public List<Todo> getAllList() {
+        return todoService.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable long id) {
+        boolean check = todoService.deleteById(id);
+        if (check) return ResponseEntity.noContent().build();
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{id}")
+    public Todo getById(@PathVariable long id) {
+        Todo todo = todoService.findById(id);
+        return todo;
+    }
 }
--- mycoolapp/src/main/java/com/fptudemy/mycoolapp/mycoolapp/services/TodoService.java
+++ mycoolapp/src/main/java/com/fptudemy/mycoolapp/mycoolapp/services/TodoService.java
@@ -1,2 +1,43 @@
-package com.fptudemy.mycoolapp.mycoolapp.services;public class TodoService {
+package com.fptudemy.mycoolapp.mycoolapp.services;
+
+import com.fptudemy.mycoolapp.mycoolapp.models.Todo;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    public static List<Todo> todos = new ArrayList();
+    public static long idCounter = 0;
+
+    static {
+        todos.add(new Todo(idCounter++, "PhucNM5", "Learn Spring Boot", new Date(), false));
+        todos.add(new Todo(idCounter++, "PhucNM6", "Learn new thing", new Date(), true));
+    }
+
+    public List<Todo> findAll() {
+        return todos;
+    }
+
+    public boolean deleteById(long id) {
+        Todo todo = findById(id);
+        if (todo != null) {
+            boolean check = todos.remove(todo);
+            return check;
+        }
+        return false;
+    }
+
+    public Todo findById(long id) {
+        for (Todo todo : todos) {
+            if (todo.getId().equals(id)) {
+                return todo;
+            }
+        }
+        return null;
+    }
+
 }
